6c6d3d54122f66ab0cd67e28bacb38e77ec2786b
ronan-f <rfegan13@gmail.com>
2020-01-21 20:24:59 -0800

    Added french greeting

--- hello/hello.go
+++ hello/hello.go
@@ -6,15 +6,23 @@ func main() {
 	fmt.Println(hello("Ronan", ""))
 }
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello"
+const spanishHelloPrefix = "Hola"
+const frenchHelloPrefix = "Bonjour"
 
 func hello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
 
-	if language == "Spanish" {
-		return "Hola " + name
+	if language == spanish {
+		return spanishHelloPrefix + " " + name
+	}
+
+	if language == french {
+		return frenchHelloPrefix + " " + name
 	}
 
 	return englishHelloPrefix + " " + name
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -7,7 +7,7 @@ func TestHello(t *testing.T) {
 	assertCorrectMessage := func(t *testing.T, got, want string) {
 		t.Helper()
 		if got != want {
-			t.Errorf("Expected %q but received %q", got, want)
+			t.Errorf("Expected %q but received %q", want, got)
 		}
 	}
 
@@ -28,6 +28,13 @@ func TestHello(t *testing.T) {
 	t.Run("Use Hola if Spanish passed as language", func(t *testing.T) {
 		got := hello("Elodie", "Spanish")
 		want := "Hola Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Use Bonjour if French passed as language", func(t *testing.T) {
+		got := hello("Emmanuel", "French")
+		want := "Bonjour Emmanuel"
 
 		assertCorrectMessage(t, got, want)
 	})
