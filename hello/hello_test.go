4b836ce6a148ea830bff9a59e016bb174cac8593
ronan-f <rfegan13@gmail.com>
2020-01-21 20:17:04 -0800

    Refactored tests to avoid duplication + improve readability

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := hello("Ronan")
-	want := "Hello Ronan"
 
-	if got != want {
-		t.Errorf("Expected %q but received %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("Expected %q but received %q", got, want)
+		}
 	}
+
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := hello("Ronan")
+		want := "Hello Ronan"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Default to 'Hello world' if no argument is passed", func(t *testing.T) {
+		got := hello("")
+		want := "Hello world"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
