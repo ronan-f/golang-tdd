493791fdcd7f81dc9e9abfd982d8fad228bd26ec
ronan-f <rfegan13@gmail.com>
2020-01-23 20:50:34 -0800

    Refactor to allow choice for repetition

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,9 +1,7 @@
 package iteration
 
-const repeatCount = 5
-
 // Repeat should return character * 5
-func Repeat(letter string) string {
+func Repeat(letter string, repeatCount int) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
 		repeated += letter
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -3,8 +3,8 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 6)
+	expected := "aaaaaa"
 
 	if repeated != expected {
 		t.Errorf("Expected %q but got %q", expected, repeated)
@@ -13,6 +13,6 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 7)
 	}
 }
