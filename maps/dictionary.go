a02dd0aa3ba61dbbef33eeaffcc582fa77786794
ronan-f <rfegan13@gmail.com>
2020-02-01 19:14:57 -0800

    Handled case when word isn't found

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,9 +1,18 @@
 package maps
 
+import "errors"
+
 // Dictionary is a custom type of map
 type Dictionary map[string]string
 
 // Search checks a dictionary for words
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+
+	definition, found := d[word]
+
+	if !found {
+		return "", errors.New("Could not find the word you were looking for")
+	}
+
+	return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,10 +5,24 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is a test"}
 
-	got := dictionary.Search("test")
-	want := "this is a test"
+	t.Run("Known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is a test"
 
-	assertString(t, got, want)
+		assertString(t, got, want)
+	})
+
+	t.Run("Unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "Could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("Expected to get an error")
+		}
+
+		assertString(t, err.Error(), want)
+	})
+
 }
 
 func assertString(t *testing.T, got, want string) {
